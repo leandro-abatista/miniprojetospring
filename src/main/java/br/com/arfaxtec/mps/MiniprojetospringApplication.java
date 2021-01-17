04e2e06edcc534cd0c48b7bcf3c4676c717072c0
lheandroaluno <leandroamorimpb@gmail.com>
2021-01-17 19:57:41 -0300

    Projeto Criado

---
+++ src/main/java/br/com/arfaxtec/mps/MiniprojetospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.arfaxtec.mps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojetospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojetospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arfaxtec/mps/MiniprojetospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arfaxtec.mps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojetospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
