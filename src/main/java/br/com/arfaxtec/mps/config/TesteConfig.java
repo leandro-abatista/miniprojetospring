099cb0c1baa441931f500cb4609245ef0676d01f
lheandroaluno <leandroamorimpb@gmail.com>
2021-01-17 21:43:17 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/arfaxtec/mps/config/TesteConfig.java
@@ -0,0 +1,29 @@
+package br.com.arfaxtec.mps.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.arfaxtec.mps.entities.Usuario;
+import br.com.arfaxtec.mps.repositorios.UsuarioRepositorio;
+
+@Configuration
+@Profile("test")
+public class TesteConfig implements CommandLineRunner{
+
+	@Autowired
+	private UsuarioRepositorio usuarioRepositorio;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		usuarioRepositorio.saveAll(Arrays.asList(u1, u2));
+		
+	}
+}
